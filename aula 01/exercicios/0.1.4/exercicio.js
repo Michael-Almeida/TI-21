497cc0976b9929296bdccdd300322eb5e7da81ba
Michael-Almeida <michaelcardoso97@gmail.com>
2022-09-02 21:27:14 -0300

    exercicio 4 revisado

--- aula 01/exercicios/0.1.4/exercicio.js
+++ aula 01/exercicios/0.1.4/exercicio.js
@@ -1,9 +1,12 @@
 function compraBananas(quantidadeAlunos, turno) {
   const pesoBananas = (90 * quantidadeAlunos) / 1000;
   const quantidadeBananasPorTurno = pesoBananas * turno;
+  const margemQuantidadeBananas =
+    quantidadeBananasPorTurno + quantidadeBananasPorTurno * 0.2;
+
   console.log(`Peso das bananas a comprar: ${pesoBananas}Kg`);
   console.log(
-    `Quantidade total a comprar de banana: ${quantidadeBananasPorTurno}Kg `
+    `Quantidade total a comprar de banana: ${margemQuantidadeBananas}Kg `
   );
 }
-compraBananas(200, 2);
+compraBananas(200, 1);
