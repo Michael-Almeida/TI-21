7b34526ce390c32ab1284be1be427c83d334aa57
Michael-Almeida <73245553+Michael-Almeida@users.noreply.github.com>
2022-09-13 16:55:54 -0300

    array anos finalizado

--- aula 01/exercicios/0.1.3/exercicio.js
+++ aula 01/exercicios/0.1.3/exercicio.js
@@ -2,10 +2,10 @@ const anos = ['2012', '2010', '2020', '2022', '2035', '2045'];
 const anoBusca = 2022;
 
 function retornaAno(anos, anoBusca) {
-  for (let i = anos; i <= anos.length; i++) {
-    if (anos <= anoBusca) {
+  for (let i = 0; i < anos.length; i++) {
+    if (anos[i] < anoBusca) {
       console.log(anos[i]);
     }
   }
 }
-retornaAno(anos, anoBusca);
+retornaAno(anos, 2022);
