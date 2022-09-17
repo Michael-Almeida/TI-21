01cf70ed748a0aedda262ea5547ad72e029e99dd
Michael-Almeida <73245553+Michael-Almeida@users.noreply.github.com>
2022-09-16 22:18:25 -0300

    finalizado

--- aula 02/exercicios/0.2.2/exercicio.js
+++ aula 02/exercicios/0.2.2/exercicio.js
@@ -1,9 +1,31 @@
 const usuario = Object.freeze({
-dados: [{
-  nome: 'Michael',
-  dataNascimento: '24/10/93',
-  
-}],
-artigosProntos: [{}],
-artigosEmCriacao: [{}]
-})
+  dadosUsuario: [
+    {
+      nome: 'João',
+      dataNascimento: '24/10/83',
+      genero: 'M',
+      nInscricao: 123456,
+      filme: 'O massacre da serra elétrica',
+    },
+  ],
+  avaliacoes: [
+    {
+      descricao: 'Não aprofunda os personagens',
+      nota: 4,
+      indicaria: 'Nao',
+    },
+    {
+      descricao:
+        'A história se arrasta e acaba ficando muito massante, mas é bem bacana os efeitos visuais ',
+      nota: 8,
+      indicaria: 'sim',
+    },
+    {
+      descricao: 'Excelentes efeitos visuais, história bem amarrada',
+      nota: 10,
+      indicaria: 'sim',
+    },
+  ],
+});
+console.log(usuario);
+//console.log(usuario.avaliacoes[0]);
