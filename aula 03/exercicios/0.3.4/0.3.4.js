869dd49a2555850dbe249b0d0b24ea3b9211c7f6
Michael-Almeida <73245553+Michael-Almeida@users.noreply.github.com>
2022-09-30 19:44:47 -0300

    1 file added

---
+++ aula 03/exercicios/0.3.4/0.3.4.js
@@ -0,0 +1,26 @@
+const arquivos = [
+  {
+    nome: 'Filmes',
+    arquivos: [
+      {
+        nome: 'Comedia',
+        arquivos: [
+          {
+            nome: 'Mr. Bean',
+          },
+          {
+            nome: '2021',
+            arquivos: [
+              {
+                nome: 'Nice Guy',
+              },
+            ],
+          },
+        ],
+      },
+      {
+        nome: 'Harry Potter Ordem da Fênix',
+      },
+    ],
+  },
+];
