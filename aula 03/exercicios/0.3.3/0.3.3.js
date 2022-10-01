e9c9e411abc8429a77196e68ae321ed1e7de7f7c
Michael-Almeida <73245553+Michael-Almeida@users.noreply.github.com>
2022-09-30 21:26:19 -0300

    switch

--- aula 03/exercicios/0.3.3/0.3.3.js
+++ aula 03/exercicios/0.3.3/0.3.3.js
@@ -13,16 +13,49 @@ const medicos = [
     nome: 'Noah',
     sobrenome: 'Lutus',
     genero: 'nao-binario',
+    especialidades: ['obstetra', 'gineco'],
   },
 ];
 
-const novoArray = medicos.map((medico) => {
+/* medicos.forEach((medico) => {
   if (medico.genero === 'feminino') {
-    medico.genero = `Dr(a) ${medico.nome} ${medico.sobrenome}`;
+    medico.apresentacao = `Dr(a) ${medico.nome} ${medico.sobrenome}`;
   } else if (medico.genero === 'masculino') {
-    medico.genero = `Dr. ${medico.nome} ${medico.sobrenome}`;
+    medico.apresentacao = `Dr. ${medico.nome} ${medico.sobrenome}`;
   } else {
-    medico.genero = `Dr(e) ${medico.nome} ${medico.sobrenome}`;
+    medico.apresentacao = `Dr(e) ${medico.nome} ${medico.sobrenome}`;
   }
-  console.log(medico.genero);
+  console.log(medico.apresentacao);
+}); */
+
+// if else
+/* const novoArray = medicos.map((medico) => {
+  const { genero } = medico;
+  const _medico = structuredClone(medico);
+  if (genero === 'feminino') {
+    _medico.apresentacao = `Dr(a) ${_medico.nome} ${_medico.sobrenome}`;
+  } else if (genero === 'masculino') {
+    _medico.apresentacao = `Dr. ${_medico.nome} ${_medico.sobrenome}`;
+  } else {
+    _medico.apresentacao = `Dr(e) ${_medico.nome} ${_medico.sobrenome}`;
+  }
+  return _medico;
 });
+console.log(novoArray);
+console.log(medicos); */
+
+const novoAray = medicos.forEach((medico) => {
+  switch (medico.genero) {
+    case 'feminino':
+      medico.apresentacao = `Dr(a) ${medico.nome} ${medico.sobrenome}`;
+      break;
+    case 'masculino':
+      medico.apresentacao = `Dr. ${medico.nome} ${medico.sobrenome}`;
+      break;
+    case 'nao-binario':
+      medico.apresentacao = `Dr(e) ${medico.nome} ${medico.sobrenome}`;
+      break;
+  }
+});
+
+console.log(medicos);
