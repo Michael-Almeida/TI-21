bcf8ba3ba712f6c68c9686ce8790ac79f34f0280
Michael-Almeida <73245553+Michael-Almeida@users.noreply.github.com>
2022-10-07 21:25:00 -0300

    exemplos

--- aula 03/exemplos/metodosArray.js
+++ aula 03/exemplos/metodosArray.js
@@ -1,4 +1,4 @@
-const array = [1100, 12, 3345, 4, 10, 11];
+/* const array = [1100, 12, 3345, 4, 10, 11];
 
 const objetos = ['bola', 'café', 'sabão', 'melancia'];
 
@@ -23,11 +23,11 @@ array.forEach((array) => console.log(array));
  */
 
 //forEach
-array.forEach((array) => {
+/* array.forEach((array) => {
   if (array > 10) {
     console.log(array);
   }
-});
+});  */
 /* 
 //filter: retorna um novo array
 //const novoArray = objetos.filter((array) => array >= 12);
@@ -38,3 +38,23 @@ const novoArray = objetos.filter(function(array){
 //rever filter
 
 //some() pelo menos algum
+
+//map() retorna um novo Array Mutado
+const pessoas = [
+  { nome: 'Michael', anoNascimento: '1995-10-24', cidades: ['Porto Alegre'] },
+  {
+    nome: 'Mathias',
+    premium: true,
+    anoNascimento: '1995-11-24',
+    cidades: ['Porto Alegre'],
+  },
+];
+
+const resultado = pessoas.map((pessoa) => {
+  let cor = 'Branco';
+  if (pessoa.premium) {
+    cor = 'Dourado';
+  }
+  return { anoNascimento: pessoa.anoNascimento, cor, nome: pessoa.nome };
+});
+console.log(resultado);
