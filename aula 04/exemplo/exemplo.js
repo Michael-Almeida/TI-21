06066b118906e821e4caa81e05be5ab76c815977
Michael-Almeida <73245553+Michael-Almeida@users.noreply.github.com>
2022-10-14 22:01:19 -0300

    exemplo

--- aula 04/exemplo/exemplo.js
+++ aula 04/exemplo/exemplo.js
@@ -1,1 +1,12 @@
 let numeros = [10, 20, 80, 90];
+
+/* const callback = (acc, next, index, array) => {
+  console.log('----------');
+  console.log(acc);
+  console.log(next);
+  console.log(index);
+  console.log(array);
+
+  return acc;
+};
+numeros.reduce(callback, 0); */
