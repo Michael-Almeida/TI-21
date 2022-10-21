c97c4263f2c29c969a64e2bd82df15059696dde4
Michael-Almeida <73245553+Michael-Almeida@users.noreply.github.com>
2022-10-21 19:22:35 -0300

    1 file modified

--- aula 04/0.4.4/0.4.4.js
+++ aula 04/0.4.4/0.4.4.js
@@ -20,3 +20,14 @@ const arquivos = [
     excluido: true,
   },
 ];
+
+arquivos.forEach((arquivo) => {
+  if (arquivo.arquivos === true) {
+    arquivo.apresentacao = `Dr(a) ${arquivo.nome} ${arquivo.sobrenome}`;
+  } else if (arquivo.genero === 'masculino') {
+    arquivo.apresentacao = `Dr. ${arquivo.nome} ${arquivo.sobrenome}`;
+  } else {
+    arquivo.apresentacao = `Dr(e) ${arquivo.nome} ${arquivo.sobrenome}`;
+  }
+  console.log(arquivo.apresentacao);
+});
